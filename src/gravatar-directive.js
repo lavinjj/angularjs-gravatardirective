5ca73739ca31d82c42a4045313ff3dbaf56f4c25
David Golding <davidgolding@me.com>
2013-06-01 03:05:02 -0500

    Refactoring directive and adding email regex validation check
    Mainly just trying to clean the redundant if-statements that could be expressed with ternary statements.

--- src/gravatar-directive.js
+++ src/gravatar-directive.js
@@ -1,49 +1,36 @@
 'use strict';
 
-/*
- * An Simple AngularJS Gravatar Directive
+/**
+ * A Simple AngularJS Gravatar Directive
  *
  * Written by Jim Lavin
  * http://codingsmackdown.tv
  *
  */
-
-angular.module('ui-gravatar', ['md5']).
-    directive('gravatarImage', ['md5', function (md5) {
-        return {
-            restrict:"EAC",
-            link:function (scope, elm, attrs) {
-                // by default the values will come in as undefined so we need to setup a
-                // watch to notify us when the value changes
-                scope.$watch(attrs.email, function (value) {
-                    // let's do nothing if the value comes in empty, null or undefined
-                    if ((value !== null) && (value !== undefined) && (value !== '') && (null != value.match(/.*@.*\..{2}/))) {
-                        // convert the value to lower case and then to a md5 hash
-                        var hash = md5.createHash(value.toLowerCase());
-                        // parse the size attribute
-                        var size = attrs.size;
-                        // default to 40 pixels if not set
-                        if((size=== null) || (size == undefined) || (size == '')){
-                            size = 40;
-                        }
-                        // parse the ratings attribute
-                        var rating = attrs.rating;
-                        // default to pg if not set
-                        if((rating === null) || (rating === undefined)|| (rating === '')){
-                            rating = 'pg';
-                        }
-                        // parse the default image url
-                        var defaultUrl = attrs.default;
-                        if((defaultUrl === null) || (defaultUrl === undefined)|| (defaultUrl === '')) {
-                            defaultUrl = '404';
-                        }
-                        // construct the tag to insert into the element
-                        var tag = '<img class="gravatar-icon" src="' + (attrs.secure ? 'https://secure' : 'http://www' ) + '.gravatar.com/avatar/' + hash + '?s=' + size + '&r=' + rating + '&d=' + defaultUrl + '" >'
-                        //remove any existing imgs 
-                         $(elm).find(".gravatar-icon").remove();           
-                        // insert the tag into the element
-                        elm.append(tag);
-                    }
-                });
-            }};
-    }]);
+angular.module('ui-gravatar', ['md5']).directive('gravatarImage', ['md5', function(md5) {
+    return {
+        restrict: 'EAC',
+    	link: function(scope, elem, attrs) {
+    		// by default the values will come in as undefined so we need to set up 
+    		// a watch to notify us when the value changes
+    		scope.$watch(attrs.email, function(value) {
+    			var empty = function(variable) {
+	    			return typeof variable !== 'undefined' ? true : false;
+    			};
+    			// Let's do nothing if the value comes in empty, null, or undefined
+    			if (empty(value) && value.match(/.*@.*\..{2}/) != null) {
+    				if (/\S+@\S+\.\S+/.test(value)) {
+    					var hash = md5.createHash(value.toLowerCase()), //convert value to lowercase, then md5 hash
+    						size = empty(attrs.size) ? 40 : attrs.size, //parse the size attribute, default to 40px
+    						rating = empty(attrs.rating) ? 'pg' : attrs.rating, //parse ratings attribute, default to 'pg'
+    						url = empty(attrs.default) ? '404' : attrs.default, //parse default image url, default to '404'
+    						tag = '<img class="gravatar-icon" src="' + (attrs.secure ? 'https://secure' : 'http://www')
+    							+ '.gravatar.com/avatar/' + hash + '?s=' + size + '&r=' + rating + '&d=' + url + '" />'; //construct the tag
+    					$(elem).find('.gravatar-icon').remove(); //remove any existing gravatar imgs
+    					elem.append(tag); //insert the tag into the element
+    				}
+    			}
+    		});
+    	}
+    }
+}]);
