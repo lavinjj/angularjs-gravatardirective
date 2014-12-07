445755d4f50b87d959a9fe46eb1956a35e247db7
goodseller <maninw@gmail.com>
2014-12-07 15:45:44 +0800

    add hash feature

--- src/angularjs-gravatardirective/services/gravatarImageService.js
+++ src/angularjs-gravatardirective/services/gravatarImageService.js
@@ -1,9 +1,14 @@
 angular.module('angularjs-gravatardirective.services')
     .factory('gravatarImageService', ['md5', function (md5) {
         return {
-            getImageSrc: function (value, size, rating, defaultUrl, secure) {
-                // convert the value to lower case and then to a md5 hash
-                var hash = md5.createHash(value.toLowerCase());
+            getImageSrc: function (value, size, rating, defaultUrl, secure, mode) {
+				if (mode === 'hash') {
+					// directly use hashed value
+					var hash = value;
+				} else {
+					// convert the value to lower case and then to a md5 hash
+					var hash = md5.createHash(value.toLowerCase());
+				}
                 var src = (secure ? 'https://secure' : 'http://www' ) + '.gravatar.com/avatar/' + hash;
                 if (size) src += '?s=' + size;
                 if (rating) src += '&r=' + rating;
