0c3cce789dad71e8f805e73b9daa9942419aeb9c
goodseller <maninw@gmail.com>
2015-04-04 22:17:38 +0800

    Add test for hash features

--- test/unit/angularjs-gravatardirective/directives/gravatar-directive-test.js
+++ test/unit/angularjs-gravatardirective/directives/gravatar-directive-test.js
@@ -1,55 +1,107 @@
 describe('gravatar directive', function() {
-    var elm;
-    var scope;
+	var elm, eleHash;
+	var scope;
 
 
-    // load the localization code
-    beforeEach(module('angularjs-gravatardirective'));
+	// load the localization code
+	beforeEach(module('angularjs-gravatardirective'));
 
-    beforeEach(inject(function ($rootScope, $compile) {
-        // we might move this tpl into an html file as well...
-        elm = angular.element('<gravatar-image data-gravatar-email="email" data-gravatar-size="120" data-gravatar-rating="pg" data-gravatar-default="identicon" data-gravatar-css-class="btn btn-large" ></gravatar-image>');
+	beforeEach(inject(function($rootScope, $compile) {
+		// we might move this tpl into an html file as well...
+		elm = angular.element(
+			'<gravatar-image data-gravatar-email="email" data-gravatar-size="120" data-gravatar-rating="pg" data-gravatar-default="identicon" data-gravatar-css-class="btn btn-large" ></gravatar-image>'
+		);
+		elmHash = angular.element(
+			'<gravatar-image data-gravatar-hash="hashedEmail" data-gravatar-size="120" data-gravatar-rating="pg" data-gravatar-default="identicon" data-gravatar-css-class="btn btn-large"></gravatar-image>'
+		);
 
-        scope = $rootScope;
-        scope.email = 'jlavin@jimlavin.net';
+		scope = $rootScope;
+		scope.email = 'jlavin@jimlavin.net';
+		scope.hashedEmail = '7a5b7242f9370beb1eda421a109d9f77';
+		$compile(elm)(scope);
+		$compile(elmHash)(scope);
 
-        $compile(elm)(scope);
-        scope.$digest();
-    }));
+		scope.$digest();
+	}));
 
-    it('should add image tag to the element', function () {
-        var image = elm.html();
+	// scope
+	it('should add image tag to the element', function() {
+		var image = elm.html();
 
-        expect(image).toContain('<img');
-    });
+		expect(image).toContain('<img');
+	});
 
-    it('should add md5 of the email address to the image tag', function () {
-        var image = elm.html();
+	it('should add md5 of the email address to the image tag', function() {
+		var image = elm.html();
 
-        expect(image).toContain('80b03752791145a3fdd027b154d7b42b');
-    });
+		expect(image).toContain('80b03752791145a3fdd027b154d7b42b');
+	});
 
-    it('should add size parameter of 120 pixels to the image tag', function () {
-        var image = elm.html();
+	it('should add size parameter of 120 pixels to the image tag', function() {
+		var image = elm.html();
 
-        expect(image).toContain('s=120');
-    });
+		expect(image).toContain('s=120');
+	});
 
-    it('should add a rating parameter of PG to the image tag', function () {
-        var image = elm.html();
+	it('should add a rating parameter of PG to the image tag', function() {
+		var image = elm.html();
 
-        expect(image).toContain('r=pg');
-    });
+		expect(image).toContain('r=pg');
+	});
 
-    it('should add a default image parameter of identicon to the image tag', function () {
-        var image = elm.html();
+	it('should add a default image parameter of identicon to the image tag',
+		function() {
+			var image = elm.html();
 
-        expect(image).toContain('d=identicon');
-    });
+			expect(image).toContain('d=identicon');
+		});
 
-    it('should add a parse the data-cc-class attribute and assign it to the class attribute', function () {
-        var cssClass = elm.html();
+	it(
+		'should add a parse the data-cc-class attribute and assign it to the class attribute',
+		function() {
+			var cssClass = elm.html();
 
-        expect(cssClass).toContain('class="btn btn-large"');
-    });
+			expect(cssClass).toContain('class="btn btn-large"');
+		});
+
+	// hash
+	it('should add image tag to the element', function() {
+		var image = elmHash.html();
+
+		expect(image).toContain('<img');
+	});
+
+	it('should add md5 of the email address to the image tag', function() {
+		var image = elmHash.html();
+
+		expect(image).toContain('7a5b7242f9370beb1eda421a109d9f77');
+	});
+
+	it('should add size parameter of 120 pixels to the image tag', function() {
+		var image = elmHash.html();
+
+		expect(image).toContain('s=120');
+	});
+
+	it('should add a rating parameter of PG to the image tag', function() {
+		var image = elmHash.html();
+
+		expect(image).toContain('r=pg');
+	});
+
+	it('should add a default image parameter of identicon to the image tag',
+		function() {
+			var image = elmHash.html();
+
+			expect(image).toContain('d=identicon');
+		});
+
+	it(
+		'should add a parse the data-cc-class attribute and assign it to the class attribute',
+		function() {
+			var cssClass = elmHash.html();
+
+			expect(cssClass).toContain('class="btn btn-large"');
+		});
+
 });
